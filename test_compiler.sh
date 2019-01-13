1e7d059ce56f7295af45f6632c9ec754dfe1aa3a
Michael Vilim <697a9b924bfa6f06a81e82975ddca4785b90a7b40d91044ce76f68d3bd65a9b8@697a9b924bfa6f06a81e82975ddca4785b90a7b40d91044ce76f68d3bd65a9b8.com>
2019-01-13 15:57:53 -0600

    Use a single compilation step with two executables

--- test_compiler.sh
+++ test_compiler.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo RUNNING WITH SYMBOL MAP OFF
 mkdir -p build;
 cd build;
-cmake -DUSE_SYMBOL_MAP=OFF .. >/dev/null 2>&1
+cmake .. >/dev/null 2>&1
 make >/dev/null 2>&1
-./main >/dev/null
 
+echo RUNNING WITH SYMBOL MAP OFF
+./main_off >/dev/null
 if [ $? -eq 0 ]; then
     echo PROGRAM EXITED SUCCESSFULLY
 else
@@ -14,10 +14,8 @@ else
 fi
 echo ""
 
+./main_on >/dev/null
 echo RUNNING WITH SYMBOL MAP ON
-cmake -DUSE_SYMBOL_MAP=ON .. >/dev/null 2>&1
-make >/dev/null 2>&1
-./main >/dev/null
 
 if [ $? -eq 0 ]; then
     echo PROGRAM EXITED SUCCESSFULLY
