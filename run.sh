f3a37ad48d4ece7b5edd3dea86861d366322a287
Michael Vilim <697a9b924bfa6f06a81e82975ddca4785b90a7b40d91044ce76f68d3bd65a9b8@697a9b924bfa6f06a81e82975ddca4785b90a7b40d91044ce76f68d3bd65a9b8.com>
2019-01-13 15:11:09 -0600

    Add Fedora 28

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 docker build -t mvilim/test_symbol_export_arch_gcc7 -f Dockerfile_arch_gcc7 .
 docker build -t mvilim/test_symbol_export_arch_gcc8 -f Dockerfile_arch_gcc8 .
 docker build -t mvilim/test_symbol_export_alpine_gcc8 -f Dockerfile_alpine_gcc8 .
+docker build -t mvilim/test_symbol_export_fedora_gcc8 -f Dockerfile_alpine_fedora8 .
+
 echo RUNNING ARCH GCC 7 TEST
 docker run mvilim/test_symbol_export_arch_gcc7 ./test_compiler.sh
 echo ""
@@ -11,4 +13,8 @@ echo ""
 
 echo RUNNING ALPINE GCC 8 TEST
 docker run mvilim/test_symbol_export_alpine_gcc8 ./test_compiler.sh
+echo ""
+
+echo RUNNING FEDORA GCC 8 TEST
+docker run mvilim/test_symbol_export_fedora_gcc8 ./test_compiler.sh
 echo ""
