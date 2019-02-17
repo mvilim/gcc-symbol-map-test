7288c7a1211b64cf4f4ee6ca665f8c0ffce1d972
Michael Vilim <michael.vilim@gmail.com>
2019-02-17 03:42:08 -0600

    Fix typo in fedora dockerfile name

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker build -t mvilim/test_symbol_export_arch_gcc7 -f Dockerfile_arch_gcc7 .
 docker build -t mvilim/test_symbol_export_arch_gcc8 -f Dockerfile_arch_gcc8 .
 docker build -t mvilim/test_symbol_export_alpine_gcc8 -f Dockerfile_alpine_gcc8 .
-docker build -t mvilim/test_symbol_export_fedora_gcc8 -f Dockerfile_alpine_fedora8 .
+docker build -t mvilim/test_symbol_export_fedora_gcc8 -f Dockerfile_fedora_gcc8 .
 
 echo RUNNING ARCH GCC 7 TEST
 docker run mvilim/test_symbol_export_arch_gcc7 ./test_compiler.sh
